6964137b8a6f27d200de1deeb0b96991c1b5df7d
Barbara Kałuża <basianiemczyk1212@gmail.com>
2021-10-06 15:05:44 +0200

    add life cycle metods to registartion component

--- src/pages/Registration.js
+++ src/pages/Registration.js
@@ -17,8 +17,20 @@ function Registration() {
   const [age, handleAgeChange] = useInput("22");
 
   const handleSubmit = (event) => {
-      event.preventDefault();
-  }
+    event.preventDefault();
+  };
+
+  useEffect(() => {
+    console.log("Hello from UseEffect");
+    //componentDidUpdate(prevProps, prevState) {
+    //     console.log("ComponentDidUpdate");
+    //   }
+    return () => {
+      //componentWillUnmount() {
+      //     console.log("ComponentWillUnmount");
+      //   }
+    };
+  }, []);
 
   return (
     <div>
@@ -46,4 +58,43 @@ function Registration() {
   );
 }
 
+// class Registration extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       name: "",
+//       surname: "",
+//       age: "",
+//     };
+//     this.handleSubmit = this.handleSubmit.bind(this);
+//   }
+//   componentDidMount() {
+//     console.log("ComponentDidMount");
+//   }
+//   componentDidUpdate(prevProps, prevState) {
+//     console.log("ComponentDidUpdate");
+//   }
+//   componentWillUnmount() {
+//     console.log("ComponentWillUnmount");
+//   }
+//   handleSubmit(event) {
+//     event.preventDefault();
+//   }
+//   handleChange = (event) => {
+//     //event.target.name
+//     const keyName = event.target.name;
+//     this.setState({ [keyName]: event.target.value});
+//   }
+//   render() {
+//     return (
+//       <form onSubmit={this.handleSubmit}>
+//         <input type="text" name="name" placeholder="Name" onChange={this.handleChange}></input>
+//         <input type="text" name="surname" placeholder="Surname" onChange={this.handleChange}></input>
+//         <input type="text" name="age" placeholder="Age" onChange={this.handleChange}></input>
+//         <button type="submit">Send</button>
+//         <button type="button"></button>
+//       </form>
+//     );
+//   }
+// }
 export default Registration;
